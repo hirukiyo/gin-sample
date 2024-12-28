0d105179f5de26b60141838232c94aad4d06e517
hirukiyo <hirukiyo@live.jp>
2024-12-28 10:51:31 +0900

    set ginmode

--- cmd/api_server/main.go
+++ cmd/api_server/main.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/gin-gonic/gin"
 
 	"ginapp/internal/app"
 )
 
+type App struct {
+	cfg *app.AppEnvironment
+	gin *gin.Engine
+}
+
 func main() {
-	env, _ := app.LoadEnvironmentFromDotenv()
-	r := gin.Default()
-	fmt.Println(env.AppName)
-	r.GET("/ping", func(c *gin.Context) {
+	env, err := app.LoadEnvironmentFromDotenv()
+	if err != nil {
+		slog.Error("environment load error.", "err", err)
+		os.Exit(1)
+	}
+
+	gin.SetMode(env.AppMode)
+	app := &App{
+		cfg: env,
+		gin: gin.Default(),
+	}
+
+	app.gin.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pongs",
 		})
 	})
-	r.Run(fmt.Sprintf("%s:%d", env.AppHost, env.AppPort)) // 0.0.0.0:8080 でサーバーを立てます。
+	app.gin.Run(fmt.Sprintf("%s:%d", env.AppHost, env.AppPort)) // 0.0.0.0:8080 でサーバーを立てます。
+}
+
+func setLogger(app *App) {
+	// loggerの設定
+
 }
--- internal/app/environment.go
+++ internal/app/environment.go
@@ -1,16 +1,23 @@
 package app
 
 import (
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
 type AppEnvironment struct {
-	AppName    string `envconfig:"APP_NAME"`
-	AppHost    string `envconfig:"APP_HOST"`
-	AppPort    int    `envconfig:"APP_PORT"`
-	AppVersion string `envconfig:"APP_VERSION"`
-	AppMode    string `envconfig:"APP_MODE"`
+	AppMode     string `envconfig:"APP_MODE"`
+	AppName     string `envconfig:"APP_NAME"`
+	AppHost     string `envconfig:"APP_HOST"`
+	AppPort     int    `envconfig:"APP_PORT"`
+	AppVersion  string `envconfig:"APP_VERSION"`
+	AppLogLevel string `envconfig:"APP_LOG_LEVEL"`
+	AppTest     string `envconfig:"APP_TEST"`
 }
 
 func LoadEnvironment() (*AppEnvironment, error) {
@@ -19,6 +26,13 @@ func LoadEnvironment() (*AppEnvironment, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// 値補正
+	fmt.Println("@@@@", env.AppMode)
+	env.AppMode = ginMode(env.AppMode)
+	fmt.Println("####", env.AppMode)
+	env.AppLogLevel = logLevel(env.AppLogLevel)
+
 	return &env, nil
 }
 
@@ -32,4 +46,27 @@ func LoadEnvironmentFromDotenv() (*AppEnvironment, error) {
 
 func LoadDotEnv() error {
 	return godotenv.Load()
+}
+
+func ginMode(m string) string {
+	// 値補正
+	if m != gin.DebugMode && m != gin.ReleaseMode {
+		m = gin.DebugMode
+	}
+	return m
+}
+
+func logLevel(l string) string {
+	appLogLevel := slog.LevelInfo
+	switch strings.ToLower(l) {
+	case "debug":
+		appLogLevel = slog.LevelDebug
+	case "info":
+		appLogLevel = slog.LevelInfo
+	case "warn":
+		appLogLevel = slog.LevelWarn
+	case "error":
+		appLogLevel = slog.LevelError
+	}
+	return appLogLevel.String()
 }
