72ef61b0a1d6ee57eb3aed1f5349b4e03a68903a
hirukiyo <hirukiyo@live.jp>
2024-12-29 19:41:04 +0900

    added migration files

---
+++ database/migrations/20241229101857_create_accounts.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts (
+    id bigint UNSIGNED AUTO_INCREMENT COMMENT 'アカウントID',
+    name varchar(255) NOT NULL COMMENT '名前',
+    email varchar(255) NOT NULL COMMENT 'メールアドレス',
+    password varchar(255) NOT NULL COMMENT 'パスワード',
+    created_at datetime DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=10000;
