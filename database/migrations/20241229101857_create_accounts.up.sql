9b1938369ca90865d5feac922cfcfe4ea7ecd5c0
hirukiyo <hirukiyo@live.jp>
2025-01-13 12:13:51 +0900

    add default value

--- database/migrations/20241229101857_create_accounts.up.sql
+++ database/migrations/20241229101857_create_accounts.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE accounts (
     id bigint UNSIGNED AUTO_INCREMENT COMMENT 'アカウントID',
-    name varchar(255) NOT NULL COMMENT '名前',
-    email varchar(255) NOT NULL COMMENT 'メールアドレス',
-    password varchar(255) NOT NULL COMMENT 'パスワード',
+    name varchar(255) NOT NULL DEFAULT '' COMMENT '名前',
+    email varchar(255) NOT NULL DEFAULT '' COMMENT 'メールアドレス',
+    password varchar(255) NOT NULL DEFAULT '' COMMENT 'パスワード',
     created_at datetime DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
     updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
     PRIMARY KEY (`id`)
