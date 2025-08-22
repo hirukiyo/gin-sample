62a25b4d8e6c641bc8931e852490cd357e143e63
hirukiyo <hirukiyo@live.jp>
2025-08-23 08:15:06 +0900

    separated middleware

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -1,21 +1,14 @@
 package apiserver
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log/slog"
-	"net/http"
-	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
-	"github.com/samber/lo"
 
 	"github.com/hirukiyo/gin-sample/apiserver/app"
 	"github.com/hirukiyo/gin-sample/apiserver/applog"
+	"github.com/hirukiyo/gin-sample/apiserver/middleware"
 	"github.com/hirukiyo/gin-sample/infra/mysql"
 	"github.com/hirukiyo/gin-sample/internal/handlers"
 )
@@ -37,7 +30,7 @@ func StartAPIServer() int {
 	// middleware
 	{
 		engine.Use(gin.Recovery())
-		engine.Use(RequestLoggerMiddleware())
+		engine.Use(middleware.RequestLoggingMiddleware([]string{"password", "secret"}))
 	}
 
 	// setup
@@ -74,99 +67,4 @@ func StartAPIServer() int {
 	}
 
 	return 0
-}
-
-func RequestLoggerMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		id := xid.New().String()
-
-		// extracting request body
-		body := []byte{}
-		if c.Request.Method == http.MethodPost || c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPatch {
-			body, _ = io.ReadAll(c.Request.Body)
-			c.Request.Body = io.NopCloser(bytes.NewReader(body))
-		}
-
-		applog.Info(c, "REQ",
-			"id", id,
-			"method", c.Request.Method,
-			"path", c.Request.URL.Path,
-			"query", c.Request.URL.RawQuery,
-			"client_ip", c.ClientIP(),
-			"real_ip", c.GetHeader("X-Real-IP"),
-			"forward-for", c.GetHeader("X-Forwarded-For"),
-			"host", c.Request.Host,
-			"user_agent", c.GetHeader("User-Agent"),
-			"referer", c.GetHeader("Referer"),
-			"body", string(body),
-		)
-
-		startTime := time.Now()
-		c.Next()
-		latency := time.Since(startTime)
-
-		applog.Info(c, "RES",
-			"id", id,
-			"status", c.Writer.Status(),
-			"size", c.Writer.Size(),
-			"latency", slog.AnyValue(latency),
-		)
-	}
-}
-
-// Middleware to log requests
-func LoggingMiddleware(maskKeys []string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var requestBody interface{}
-		bodyBytes, _ := io.ReadAll(c.Request.Body)
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // Restore the body for the handler
-
-		contentType := c.GetHeader("Content-Type")
-		if strings.Contains(contentType, "application/json") {
-			if err := json.Unmarshal(bodyBytes, &requestBody); err == nil {
-				requestBody = maskAndTruncateJSON(requestBody, maskKeys)
-			} else {
-				requestBody = string(bodyBytes)
-			}
-		} else {
-			requestBody = string(bodyBytes)
-		}
-
-		log := fmt.Sprintf("Method: %s, Path: %s, Query: %s, Body: %v",
-			c.Request.Method,
-			c.Request.URL.Path,
-			c.Request.URL.RawQuery,
-			requestBody,
-		)
-		fmt.Println(log)
-
-		c.Next()
-	}
-}
-
-func maskAndTruncateJSON(data interface{}, maskKeys []string) interface{} {
-	switch v := data.(type) {
-	case map[string]interface{}:
-		result := make(map[string]interface{})
-		for key, value := range v {
-			if lo.Contains(maskKeys, key) {
-				result[key] = "***MASKED***"
-			} else {
-				result[key] = maskAndTruncateJSON(value, maskKeys)
-			}
-		}
-		return result
-	case []interface{}:
-		for i, value := range v {
-			v[i] = maskAndTruncateJSON(value, maskKeys)
-		}
-		return v
-	case string:
-		if len(v) > 256 {
-			return v[:256]
-		}
-		return v
-	default:
-		return v
-	}
 }
---
+++ apiserver/middleware/request_logging.go
@@ -0,0 +1,112 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hirukiyo/gin-sample/apiserver/applog"
+	"github.com/rs/xid"
+	"github.com/samber/lo"
+)
+
+func RequestLoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := xid.New().String()
+
+		// extracting request body
+		body := []byte{}
+		if c.Request.Method == http.MethodPost || c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPatch {
+			body, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewReader(body))
+		}
+
+		applog.Info(c, "REQ",
+			"id", id,
+			"method", c.Request.Method,
+			"path", c.Request.URL.Path,
+			"query", c.Request.URL.RawQuery,
+			"client_ip", c.ClientIP(),
+			"real_ip", c.GetHeader("X-Real-IP"),
+			"forward-for", c.GetHeader("X-Forwarded-For"),
+			"host", c.Request.Host,
+			"user_agent", c.GetHeader("User-Agent"),
+			"referer", c.GetHeader("Referer"),
+			"body", string(body),
+		)
+
+		startTime := time.Now()
+		c.Next()
+		latency := time.Since(startTime)
+
+		applog.Info(c, "RES",
+			"id", id,
+			"status", c.Writer.Status(),
+			"size", c.Writer.Size(),
+			"latency", slog.AnyValue(latency),
+		)
+	}
+}
+
+// Middleware to log requests
+func RequestLoggingMiddleware(maskKeys []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var requestBody interface{}
+		bodyBytes, _ := io.ReadAll(c.Request.Body)
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // Restore the body for the handler
+
+		contentType := c.GetHeader("Content-Type")
+		if strings.Contains(contentType, "application/json") {
+			if err := json.Unmarshal(bodyBytes, &requestBody); err == nil {
+				requestBody = maskAndTruncateJSON(requestBody, maskKeys)
+			} else {
+				requestBody = string(bodyBytes)
+			}
+		} else {
+			requestBody = string(bodyBytes)
+		}
+
+		log := fmt.Sprintf("Method: %s, Path: %s, Query: %s, Body: %v",
+			c.Request.Method,
+			c.Request.URL.Path,
+			c.Request.URL.RawQuery,
+			requestBody,
+		)
+		fmt.Println(log)
+
+		c.Next()
+	}
+}
+
+func maskAndTruncateJSON(data interface{}, maskKeys []string) interface{} {
+	switch v := data.(type) {
+	case map[string]interface{}:
+		result := make(map[string]interface{})
+		for key, value := range v {
+			if lo.Contains(maskKeys, key) {
+				result[key] = "***MASKED***"
+			} else {
+				result[key] = maskAndTruncateJSON(value, maskKeys)
+			}
+		}
+		return result
+	case []interface{}:
+		for i, value := range v {
+			v[i] = maskAndTruncateJSON(value, maskKeys)
+		}
+		return v
+	case string:
+		if len(v) > 256 {
+			return v[:256]
+		}
+		return v
+	default:
+		return v
+	}
+}
